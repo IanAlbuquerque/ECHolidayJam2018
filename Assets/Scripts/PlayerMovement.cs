41b8fab0262336f44fd12bbbec3f65ceb7fe0441
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2018-12-13 22:47:37 -0200

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    public float movementSpeed = 2.0f;
+    private Vector2 currentSpeed = new Vector2(0.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 goalSpeed = direction * this.movementSpeed * Time.deltaTime; 
+        rigidbody2D.velocity = goalSpeed;
+    }
+}
