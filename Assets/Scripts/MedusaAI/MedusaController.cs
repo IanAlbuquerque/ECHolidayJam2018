43495b48d335c484927b10f072cbdcaf5e58dd7f
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2018-12-16 19:38:55 -0200

    medusa full fight

--- Assets/Scripts/MedusaAI/MedusaController.cs
+++ Assets/Scripts/MedusaAI/MedusaController.cs
@@ -10,13 +10,14 @@ public class MedusaController : MonoBehaviour
     private float counter = 0.0f;
     private int currentPattern = 0;
 
+    private int phase = 0;
+
     public void Start() {
-        this.patterns.Add(this.GetComponent<MedusaWanderWithGas>());
-        this.durations.Add(20.0f);
+        this.phase = 0;
+        this.currentPattern = 0;
+        this.counter = 0.0f;
         this.patterns.Add(this.GetComponent<MedusaWander>());
-        this.durations.Add(20.0f);
-        this.patterns.Add(this.GetComponent<MedusaFirework>());
-        this.durations.Add(20.0f);
+        this.durations.Add(10.0f);
         this.patterns[this.currentPattern].startAttackPattern();
     }
 
@@ -26,6 +27,18 @@ public class MedusaController : MonoBehaviour
             this.counter = 0.0f;
             this.patterns[this.currentPattern].stopAttackPattern();
             this.currentPattern = (this.currentPattern + 1) % this.patterns.Count;
+            this.patterns[this.currentPattern].startAttackPattern();
+        }
+        if(BossHealth.Instance.hp / BossHealth.Instance.maxHp <= 0.5f && this.phase == 0) {
+            this.currentPattern = 0;
+            this.counter = 0.0f;
+            this.phase = 1;
+            this.patterns[this.currentPattern].stopAttackPattern();
+            this.patterns = new List<EnemyAttackPattern>();
+            this.patterns.Add(this.GetComponent<MedusaFirework>());
+            this.durations.Add(30.0f);
+            this.patterns.Add(this.GetComponent<MedusaWanderWithGas>());
+            this.durations.Add(30.0f);
             this.patterns[this.currentPattern].startAttackPattern();
         }
     }
