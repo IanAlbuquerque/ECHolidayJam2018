a7000b543998500ef2ab9f952c5dee4ae57b57cb
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2018-12-18 02:07:17 -0200

    background

---
+++ Assets/Scripts/GameObjectKeeper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectKeeper : MonoBehaviour
+{
+    public GameObject storedGameObject;
+
+    public GameObject initialGameObject;
+
+    void Start () {
+        this.storedGameObject = initialGameObject;
+    }
+}
---
+++ Assets/Scripts/MoveLeftAndLoopBack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftAndLoopBack : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    private SpriteRenderer spriteRenderer;
+    public float limitLeft = -4.0f;
+
+    public GameObjectKeeper keeper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+        pos.x -= this.speed * Time.deltaTime;
+        if(pos.x + (this.spriteRenderer.bounds.size.x / 2.0f) < this.limitLeft) {
+            SpriteRenderer keeperSpriteRenderer = this.keeper.storedGameObject.transform.GetComponent<SpriteRenderer>();
+            float storedX = this.keeper.storedGameObject.transform.position.x;
+            float halfSizeStored = (keeperSpriteRenderer.bounds.size.x / 2.0f);
+            pos.x = storedX + halfSizeStored * 2.0f - this.speed * Time.deltaTime;// + (this.spriteRenderer.bounds.size.x * this.transform.localScale.x * this.keeper.gameObject.transform.localScale.x / 4.0f);
+            keeper.storedGameObject = this.gameObject;
+        }
+        this.transform.position = pos;
+    }
+}
